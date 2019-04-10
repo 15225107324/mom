e096687ad1056a03c8e537ac3a7cac1c02f9ec34
15225107324 <m15538328617@163.com>
2019-04-10 17:14:12 +0800

    1

---
+++ 中心环境---展示/JS/five.js
@@ -0,0 +1,10 @@
+var move=document.getElementsByClassName("botton_two");
+move.onclick=function(){
+	 window.scrollT(0,400);
+}
+var btn=document.getElementById("left");
+btn.onclick=function(){
+	 document.getElementById("img").src ="E:/img/5_10.jpg"
+}
+
+
